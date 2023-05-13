cd0dae41def50c39fae54f6788e1824f9ca1b93f
BigSpaces <info@ithesound.com>
2023-05-13 23:51:19 +0200

    Attempted testing embedding chunks. Requires more work. Not yet pass

--- test/documentation/self_documenting.exs
+++ test/documentation/self_documenting.exs
@@ -8,6 +8,25 @@ defmodule SelfDocumenting do
   alias LangChain.Retriever
   alias LangChain.Retriever.FileSystemProvider
 
+  alias LangChain.EmbedderProtocol
+  alias LangChain.LanguageModelProtocol
+  alias LangChain.Providers.Huggingface.Embedder
+  alias LangChain.Providers.Huggingface.LanguageModel
+
+  @model %LanguageModel{
+    model_name: "gpt2"
+  }
+  @ms_gpt_model %LanguageModel{
+    model_name: "microsoft/DialoGPT-large"
+  }
+  @embedder_gpt2 %Embedder{
+    # gpt2"
+    model_name: "sentence-transformers/distilbert-base-nli-mean-tokens"
+  }
+  @embedder_ms_gpt %Embedder{
+    model_name: "microsoft/DialoGPT-large"
+  }
+
   test "turn self into text chunks" do
     # get the contents of all files under the /lib directory
     path = Path.dirname(__ENV__.file) |> Path.dirname() |> Path.dirname() |> Path.join("lib")
@@ -26,5 +45,22 @@ defmodule SelfDocumenting do
   end
 
   test "turn self into text chunks and then embed them" do
+     # get the contents of all files under the /lib directory
+    path = Path.dirname(__ENV__.file) |> Path.dirname() |> Path.dirname() |> Path.join("lib")
+    query = %{path: path, recursive: true}
+    file_system_reader = %FileSystemProvider{}
+    project_source_code = Retriever.get_relevant_documents(file_system_reader, query)
+    # now split them into chunks and with the newline character as the separator
+    splitter = %Character{
+      embedder_name: "gpt2",
+      chunk_size: 768,
+      chunk_overlap: 200,
+      separator: "\r\n"
+    }
+    chunks = TextSplitter.split_strings(splitter, project_source_code)
+    EmbedderProtocol.embed_documents(@embedder_gpt2, [
+      "What time is it now?",
+      "Fourscore and seven years ago"
+    ]) |> IO.inspect() #chunks
   end
 end
