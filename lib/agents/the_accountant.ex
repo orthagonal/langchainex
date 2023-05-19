84cae337d8a3f5d92b9639719ad9cee947ec5467
orthagonal <orthagonal1@gmail.com>
2023-05-19 01:19:53 -0500

    Update the_accountant.ex

--- lib/agents/the_accountant.ex
+++ lib/agents/the_accountant.ex
@@ -5,7 +5,7 @@ defmodule LangChain.Agents.TheAccountant do
   """
   use GenServer
 
-  def start_link() do
+  def start_link do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
